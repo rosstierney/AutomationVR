64463bebbbebad0b1129fabf8fba1a153c852da7
Ross Tierney <rossgtierney@gmail.com>
2017-10-27 18:44:34 +0100

    Added .webm video to the scene

--- index.vr.js
+++ index.vr.js
@@ -5,6 +5,9 @@ import {
   Pano,
   Text,
   View,
+  Video,
+  MediaPlayerState,
+  VideoControl,
 } from 'react-vr';
 import GazeButton from 'react-vr-gaze-button'
 
@@ -14,7 +17,8 @@ export default class AutomationVR extends React.Component {
     {
         super(props)
         this.state = {
-            buttonIsClicked: false
+            buttonIsClicked: false,
+            playerState: new MediaPlayerState({autoPlay: true, muted: true}), // init with muted, autoPlay
         };
     }
   render() {
@@ -24,13 +28,22 @@ export default class AutomationVR extends React.Component {
     return (
       <View>
         <Pano source={asset('office.jpg')}/>
-        <GazeButton onClick={() => this.setState({buttonIsClicked: true})} duration={2000}>
+
+        <Video
+               style={{position: 'absolute', height: 2.5, width: 4, layoutOrigin: [1.5, 0.45], transform: [{translate: [0, 0, -2.8]}, {rotateY: -270}]}}
+               source={asset('big-buck-bunny.webm')}
+               playerState={this.state.playerState} />
+        <VideoControl
+               style={{position: 'absolute', height: 0.2, width: 4, layoutOrigin: [1.5, -6.2], transform: [{translate: [0, 0, -2.8]}, {rotateY: -270}]}}
+               playerState={this.state.playerState} />
+
+        <GazeButton onClick={() => this.setState({buttonIsClicked: true})} duration={5000}>
                   {time => (
                     <Text style={{
                                       backgroundColor: '#000',
                                       fontSize: 0.8,
                                       fontWeight: '400',
-                                      layoutOrigin: [0.5, 0.5],
+                                      layoutOrigin: [0.5, 1.5],
                                       paddingLeft: 0.2,
                                       paddingRight: 0.2,
                                       textAlign: 'center',
